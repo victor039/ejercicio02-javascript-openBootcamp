4b4523d3d3937689874eb235bfb43ffbd61930d6
victor <vmvg08@hotmail.com>
2023-03-31 11:49:19 -0500

    ejercicio 02 js openbootcamp

---
+++ ejercicio-02.js
@@ -0,0 +1,14 @@
+"use strict"
+
+const datosPersonales = ["victor", 39, true, new Date("1984-02-09") ]
+console.log(datosPersonales);
+
+
+const libro = {
+    titulo: "El poder invisible de la visualizació",
+    autor: "Genevieve Behrend",
+    fecha: new Date("2019-06-14"),
+    url: "https://docs.google.com/viewer?a=v&pid=sites&srcid=ZGVmYXVsdGRvbWFpbnxwdWVydGFzYXN0cmFsZXN8Z3g6M2Q5ZjlmZTIyZDkyZWI1Nw"
+}
+
+console.log(libro);
